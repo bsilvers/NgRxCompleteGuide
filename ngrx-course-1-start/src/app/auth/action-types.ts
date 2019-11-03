2a8f54fa7a3cfadb7e1f65db18fcecfd522dc023
Brian Silvers <brian.silvers@aa.com>
2019-11-03 17:31:07 -0600

    NgRx progress

---
+++ ngrx-course-1-start/src/app/auth/action-types.ts
@@ -0,0 +1,4 @@
+import * as AuthActions from './auth.actions';
+// allows us to us all the authentication actions just from importing AuthActions
+// by grouping the authentication actions together
+export {AuthActions};
